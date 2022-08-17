27a9a0ab6aa302de7408e008ca7240dc3897c1c8
Santiago Trabucco <santiagotrabucco@gmail.com>
2022-08-16 23:55:38 -0300

    Finish Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import style from '../styles/Footer.module.css'
+import linkedin from '../assets/linkedin.svg'
+import github from '../assets/github.svg'
 
 const Footer = () => {
   return (
     <div className={style.footer}>
       <p>Creado por Santiago Trabucco</p>
       <div className={style.links}>
-        <p>LinkedIn</p>
+        <a href='https://github.com/Sandotcom' target="_blank" rel="noreferrer noopener">
+          <img src={github} alt='github'/>
+        </a>
+        <a href='https://www.linkedin.com/in/santiagotrabucco/' target="_blank" rel="noreferrer noopener">
+          <img src={linkedin} alt='linkedin' />
+        </a>
       </div>
     </div>
   )
